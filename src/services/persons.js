5a81cfb54b950fd45173eec37c4d356749382304
Shraddha-sutar <shraddhasutar133@gmail.com>
2025-10-25 12:52:09 +0530

    Updated API base URL for Render

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,23 +1,34 @@
 import axios from 'axios'
 
-// Backend URL 
+// Backend URL (तुमच्या Render-deployed backend URL नुसार बदला)
 const baseUrl = 'https://phonebook-backend-1-2eqv.onrender.com/api/persons'
 
-// API functions
+// GET all persons
 const getAll = () => {
   return axios.get(baseUrl)
+    .then(response => {
+      // response.data array आहे का ते check करा
+      const data = Array.isArray(response.data) ? response.data : [];
+      return data;
+    })
 }
 
+// CREATE new person
 const create = (newPerson) => {
   return axios.post(baseUrl, newPerson)
+    .then(response => response.data)
 }
 
+// UPDATE existing person
 const update = (id, updatedPerson) => {
   return axios.put(`${baseUrl}/${id}`, updatedPerson)
+    .then(response => response.data)
 }
 
+// DELETE person
 const remove = (id) => {
   return axios.delete(`${baseUrl}/${id}`)
+    .then(response => response.data)
 }
 
 // Default export for easy import
